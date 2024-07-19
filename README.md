a0000b2fda0b7c06331124a9ef479da2ea85dc47
RCamilo12 <rozocamilo22@gmail.com>
2024-07-19 11:17:29 -0500

    Part 1 solved

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # leaflet-challenge
+In this project we create a map to show the differents earthquakes that have been reported in the past 7days accross the world. The used data belongs to the USGS and it can be found in this link: 
+
+-https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+
+There were four functions created for this process:
+- createMap function that allowed us to create all the layers and display our map.
+- legends funcion allowed us to create the legend of the map by looping the data through some intervals and creating labels for it.
+- getColor function allowed us to sort the depth values into different colors to display.
+- createMarkers function which loops through the USGS's API to obtain the points and information for the markers.
+
+
+Part two of this challenge was attemped fetching the GeoJSON file of the tectonic plates, however an CORS error was found in the console and I was not able to fix it.
---
+++ static/js/logic.js
@@ -0,0 +1,104 @@
+
+function createMap(earthquakes) {
+
+    // Create the tile layer that will be the background of our map.
+    let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    // Create a baseMaps object to hold the streetmap layer.
+    let baseMaps = {
+      "Street Map": streetmap,
+    };
+    // Create an overlayMaps object to hold the earthquakes layer.
+    let overlayMaps = {
+      "Earthquakes": earthquakes
+    };
+
+    // Create the map object with options.
+    let map = L.map("map", {
+      center: [37.0902, -95.7129],
+      zoom: 5,
+      layers: [streetmap, earthquakes]
+    });
+    // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(map);
+      // Create a legend control
+      let legend = L.control({position: 'bottomright'});
+
+      legend.onAdd = function () {
+          let div = L.DomUtil.create('div', 'info legend'),
+              depths = [0, 5, 10, 30, 70, 100],
+              labels = [];
+
+          // Add a title to the legend
+          div.innerHTML += '<p>Depth of the earthquake in km</p>';
+  
+          // Loop through depth intervals and generate a label with a colored square for each interval
+          for (let i = 0; i < depths.length; i++) {
+              div.innerHTML +=
+                  '<i style="background:' + getColor(depths[i]) + '"></i> ' +
+                  depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
+          }
+  
+          return div;
+      };
+  
+      // Add the legend to the map
+      legend.addTo(map);
+  }
+  
+  // Define getColor function to return color based on depth
+function getColor(depth) {
+  return depth > 100 ? "#d73027" : //Here we add colors from green to red to each possible value of the depth.
+         depth > 70 ? "#fc8d59" :   //Colors were grabbed from https://colorbrewer2.org/#type=sequential&scheme=YlOrRd&n=6
+         depth > 30  ? "#fee08b" :
+         depth > 10  ? "#d9ef8b" :
+         depth > 5  ? "#91cf60" :
+                       "#1a9850";
+}
+
+function createMarkers(response) {
+
+    // Pull the properties from response.data.
+    let events = response.features;
+  
+    // Initialize an array to hold markers.
+    let earthquakeMarkers = [];
+
+    // Loop through the properties "events" array.
+    for (let index = 0; index < events.length; index++) {
+      let coordinates = events[index].geometry.coordinates; //we create the coordinates function with the API info
+      let mag = events[index].properties.mag; //Bringing the magnitude to the loop
+      let radius = mag * 20000; //We Amplify the radius of the marker to make it more visible
+      let depth1 = coordinates[2]; //We bring the depth from the API
+      let depth = depth1*10 //Amplify the depth, once again to make it more visible
+      let place = events[index].properties.place //Info for the pop up
+      let url = events[index].properties.url //Info for the pop up
+      
+      // Print depth value to the console
+      console.log(`Earthquake depth: ${depth} km`); //I printed this to give myself an idea of the depth values and create the range for colors
+
+      let color = getColor(depth); //We create a color variable depending on the function created above
+        
+      //Create a marker per event, and bind a popup with the the API info.
+      let earthquakeMarker = L.circle([coordinates[1], coordinates[0]],{
+        color: color,
+        fillColor: color,
+        fillOpacity: color,
+        radius: radius
+      })
+        .bindPopup(`<h3>${place}</h3><br><p>Magnitude: ${mag}<br>Depth: ${depth}<br>More Information: ${url}</p>`);
+  
+      // Add the marker to the array.
+      earthquakeMarkers.push(earthquakeMarker);
+    }
+  
+    // Create a layer group and pass it to the createMap function.
+    createMap(L.layerGroup(earthquakeMarkers));
+  }
+
+
+  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
